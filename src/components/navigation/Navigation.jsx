ea27f8d36552dc7bf4631245a66a1c3d31be773b
Lesleyvm <lvanmusscher@gmail.com>
2023-09-21 17:51:04 +0200

    practice done

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import './Navigation.css';
+import {NavLink} from "react-router-dom";
 
 function Navigation() {
-  return (
-    <nav>
-      <div className="nav-container">
-        <h4>De Tandenborstel</h4>
-
-        <ul>
-          <li>Home</li>
-          <li>Gaatjes</li>
-          <li>Afspraak maken</li>
-          <li>Tanden bleken</li>
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav>
+            <div className="nav-container">
+                <h4>De Tandenborstel</h4>
+                <ul>
+                    <li>
+                        <NavLink  to="/"
+                                 className={({isActive}) => isActive ? 'active-link' : 'default-link'}>Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink  to="/cavities"
+                                 className={({isActive}) => isActive ? 'active-link' : 'default-link'}>Gaatjes
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink  to="/appointments"
+                                 className={({isActive}) => isActive ? 'active-link' : 'default-link'}>Afspraak maken
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="whitening"
+                                 className={({isActive}) => isActive ? 'active-link' : 'default-link'}>Tanden bleken
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
